3af6caf6e5d408e6a320a25db04a307a728328da
Davud Samed Tombul <55891311+ProgrammerAnchowy@users.noreply.github.com>
2021-01-27 12:20:38 +0300

    Calculating given mathematical formula

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace _1D_B171210007_NDP_Odev3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DuyuruLabel = new System.Windows.Forms.Label();
+            this.BaslıkLabel = new System.Windows.Forms.Label();
+            this.SonucText = new System.Windows.Forms.TextBox();
+            this.HesaplaButon = new System.Windows.Forms.Button();
+            this.HesapText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // DuyuruLabel
+            // 
+            this.DuyuruLabel.AutoSize = true;
+            this.DuyuruLabel.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.DuyuruLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.DuyuruLabel.Location = new System.Drawing.Point(287, 163);
+            this.DuyuruLabel.Name = "DuyuruLabel";
+            this.DuyuruLabel.Size = new System.Drawing.Size(225, 20);
+            this.DuyuruLabel.TabIndex = 10;
+            this.DuyuruLabel.Text = "Hesabı Yapılacak İşlemi Giriniz";
+            // 
+            // BaslıkLabel
+            // 
+            this.BaslıkLabel.BackColor = System.Drawing.Color.YellowGreen;
+            this.BaslıkLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.BaslıkLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Millimeter, ((byte)(162)));
+            this.BaslıkLabel.Location = new System.Drawing.Point(97, 50);
+            this.BaslıkLabel.Name = "BaslıkLabel";
+            this.BaslıkLabel.Size = new System.Drawing.Size(607, 54);
+            this.BaslıkLabel.TabIndex = 9;
+            this.BaslıkLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // SonucText
+            // 
+            this.SonucText.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.SonucText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.SonucText.Location = new System.Drawing.Point(322, 378);
+            this.SonucText.Name = "SonucText";
+            this.SonucText.Size = new System.Drawing.Size(164, 23);
+            this.SonucText.TabIndex = 8;
+            this.SonucText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // HesaplaButon
+            // 
+            this.HesaplaButon.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.HesaplaButon.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.HesaplaButon.Location = new System.Drawing.Point(291, 285);
+            this.HesaplaButon.Name = "HesaplaButon";
+            this.HesaplaButon.Size = new System.Drawing.Size(219, 57);
+            this.HesaplaButon.TabIndex = 7;
+            this.HesaplaButon.UseVisualStyleBackColor = false;
+            this.HesaplaButon.Click += new System.EventHandler(this.HesaplaButon_Click);
+            // 
+            // HesapText
+            // 
+            this.HesapText.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.HesapText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.HesapText.Location = new System.Drawing.Point(291, 218);
+            this.HesapText.Name = "HesapText";
+            this.HesapText.ShortcutsEnabled = false;
+            this.HesapText.Size = new System.Drawing.Size(219, 23);
+            this.HesapText.TabIndex = 6;
+            this.HesapText.Click += new System.EventHandler(this.HesapText_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DuyuruLabel);
+            this.Controls.Add(this.BaslıkLabel);
+            this.Controls.Add(this.SonucText);
+            this.Controls.Add(this.HesaplaButon);
+            this.Controls.Add(this.HesapText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label DuyuruLabel;
+        private System.Windows.Forms.Label BaslıkLabel;
+        private System.Windows.Forms.TextBox SonucText;
+        private System.Windows.Forms.Button HesaplaButon;
+        private System.Windows.Forms.TextBox HesapText;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿/****************************************************************************
+**					    SAKARYA ÜNİVERSİTESİ
+**				BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+**				    BİLGİSAYAR MÜHENDİSLİĞİ BÖLÜMÜ
+**				   NESNEYE DAYALI PROGRAMLAMA DERSİ
+**					    2018-2019 BAHAR DÖNEMİ
+**	
+**				ÖDEV NUMARASI..........: 3
+**				ÖĞRENCİ ADI............: Davud Samed Tombul
+**				ÖĞRENCİ NUMARASI.......: B171210007
+**              DERSİN ALINDIĞI GRUP...: 1D
+****************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1D_B171210007_NDP_Odev3
+{
+    public partial class Form1 : Form
+    {
+
+        string metin = "Hesaplanacak işlemi giriniz...";
+        public Form1()
+        {
+            InitializeComponent();
+
+            HesapText.Text = metin; //HesaplaText adlı textbox'a 'metin' adlı değişken içerisindeki string değer atanıyor.
+
+            AcceptButton = HesaplaButon; //Enter tuşuna basıldığında HesaplaButon adlı butonun çalışmasını sağlıyor.
+
+            SonucText.Text = "SONUÇ"; //SonucText adlı textbox a Sonuç yazdırılıyor.
+
+            HesaplaButon.Text = "Hesapla"; //HesaplaButon adlı butonun metnine Hesapla yazdırılıyor.
+
+            BaslıkLabel.Text = "Hesap Programı"; //Başlık içi kullandığımız BaslıkLabel adlı labele Hesap Programı yazdırılıyor.
+
+        }
+
+        private void HesapText_Click(object sender, EventArgs e)//HesapText adlı işlemleri girdiğimiz textbox'a tıklayınca bu  fonksiyon çalışıyor.
+        {
+            HesapText.Text = "";//HesapText adlı textbox'un metni temizleniyor.
+        }
+
+        private void HesaplaButon_Click(object sender, EventArgs e)//HesaplaButon adlı butonumuza basıldığında bu fonksiyon çalışıp gerekli işlemler yapılıyor.
+        {
+            var hesaplama = new DataTable();
+            string yazdir = HesapText.Text;
+            string sonuc = Convert.ToString(hesaplama.Compute(yazdir.Trim(), null));//Hesaplamalar yapılıp forma yazdırmak için uygun veri tipine yani stringe çeviriliyor.
+            SonucText.Text = sonuc; //SonucText adlı işlem sonucunun yazdığı textbox'a sonuc değişkenimizdeki değer atanıyor.
+        }
+    }
+}
